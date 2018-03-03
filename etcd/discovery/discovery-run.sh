64cf78ed72d4125a758c09d3d21b5e871376ab43
yangjie <jie_071218@163.com>
2018-03-03 17:02:46 +0800

    引入变量

--- etcd/discovery/discovery-run.sh
+++ etcd/discovery/discovery-run.sh
@@ -2,12 +2,12 @@
 #
 discovery_url="$1"
 
-if [[ ! $discovery_url ]];then
-   echo "请输入discovery_url..."
+if [[ ! $discovery_url ]] || [[ ! $ETCD_ID ]];then
+   echo "请输入id 和 discovery_url..."
    exit
 fi
 
-echo "\033[33metcd --name etcd1 \
+echo -e "\033[33metcd --name etcd${ETCD_ID} \
 --data-dir /var/lib/etcd \
 --initial-advertise-peer-urls http://0.0.0.0:2380 \
 --listen-peer-urls http://0.0.0.0:2380 \
@@ -15,7 +15,7 @@ echo "\033[33metcd --name etcd1 \
 --advertise-client-urls http://0.0.0.0:2379 \
 --discovery ${discovery_url}\033[0m"
 
-etcd --name etcd1 \
+etcd --name etcd${ETCD_ID} \
 --data-dir /var/lib/etcd \
 --initial-advertise-peer-urls http://0.0.0.0:2380 \
 --listen-peer-urls http://0.0.0.0:2380 \
