f6618135de6c44f1e9546223b0353cd3a504b078
yangjie <jie_071218@163.com>
2018-03-03 17:35:13 +0800

    使用固定ip

--- etcd/discovery/discovery-run.sh
+++ etcd/discovery/discovery-run.sh
@@ -7,18 +7,22 @@ if [[ ! $discovery_url ]] || [[ ! $ETCD_ID ]];then
    exit
 fi
 
+if [[ ! $HOST ]];then
+   HOST=`/sbin/ifconfig |grep '192.168.99'|awk '{print $2}'|awk -F':' '{print $NF}'`
+fi
+
 echo -e "\033[33metcd --name etcd${ETCD_ID} \
 --data-dir /var/lib/etcd \
---initial-advertise-peer-urls http://0.0.0.0:2380 \
---listen-peer-urls http://0.0.0.0:2380 \
---listen-client-urls http://0.0.0.0:2379 \
---advertise-client-urls http://0.0.0.0:2379 \
+--initial-advertise-peer-urls http://$HOST:2380 \
+--listen-peer-urls http://$HOST:2380 \
+--listen-client-urls http://$HOST:2379 \
+--advertise-client-urls http://$HOST:2379 \
 --discovery ${discovery_url}\033[0m"
 
 etcd --name etcd${ETCD_ID} \
 --data-dir /var/lib/etcd \
---initial-advertise-peer-urls http://0.0.0.0:2380 \
---listen-peer-urls http://0.0.0.0:2380 \
---listen-client-urls http://0.0.0.0:2379 \
---advertise-client-urls http://0.0.0.0:2379 \
+--initial-advertise-peer-urls http://$HOST:2380 \
+--listen-peer-urls http://$HOST:2380 \
+--listen-client-urls http://$HOST:2379,http://127.0.0.1:2379 \
+--advertise-client-urls http://$HOST:2379 \
 --discovery ${discovery_url}
